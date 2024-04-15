6940b95e822850076654efc8d734c8ae2f1ac5f0
estersdecastro <ester.castro@icen.ufpa.br>
2024-04-15 18:07:09 -0300

    update config.php

--- config.php
+++ config.php
@@ -5,17 +5,16 @@ $dbUrl = '';
 $dbParts = parse_url($dbUrl);
 
 $dbData = new stdClass();
-$dbData->driver = 'pgsql';
-$dbData->host = 'db'; // Substitua 'nome_ou_ip_do_conteiner' pelo nome ou IP do seu contêiner Docker
-$dbData->port = 4000; // Porta padrão para PostgreSQL
-$dbData->dbname = 'bdufpa'; // Nome do banco de dados
-$dbData->user = 'bdufpa'; // Substitua 'nome_do_usuario' pelo nome do usuário do seu banco de dados
-$dbData->password = 'admin'; // Substitua 'senha' pela senha do seu banco de dados
+$dbData->driver = getenv('DB_DRIVER');
+$dbData->host = getenv('DB_HOST');
+$dbData->port = getenv('DB_PORT');
+$dbData->dbname = getenv('DB_NAME');
+$dbData->user = getenv('DB_USER');
+$dbData->password = getenv('DB_PASSWORD');
 $dbData->options = [
-
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
-$dbData->connection = new PDO($dbData->driver . ':host=' . $dbData->host . ';port=5432;dbname=' . $dbData->dbname, $dbData->user, $dbData->password);
+$dbData->connection = new PDO($dbData->driver . ':host=' . $dbData->host . ';port=5432;dbname=' . $dbData->dbname, $dbData->user, $dbData->password, $dbData->options);
 ?>
