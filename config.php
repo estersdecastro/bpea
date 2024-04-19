530cced57de2d51e5d8e90db06729871d8c69169
Ester Castro <111320314+estersdecastro@users.noreply.github.com>
2024-04-19 10:37:09 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:bdufpa-server.database.windows.net,1433; Database = bdufpa", "bdufpa", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+session_start();
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "bdufpa", "pwd" => "Mestreemen$ino2024", "Database" => "bdufpa", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:bdufpa-server.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$dbData = new stdClass();
+$dbData->host = 'bdufpa-server.database.windows.net"'; // Substitua pelo host do seu banco de dados
+$dbData->driver = 'pgsql'; // Substitua pelo driver do seu banco de dados
+$dbData->port = '1433'; // Substitua pela porta do seu banco de dados
+$dbData->dbname = 'bdufpa'; // Substitua pelo nome do seu banco de dados
+$dbData->user = 'bdufpa'; // Substitua pelo usuário do seu banco de dados
+$dbData->password = 'Mestreemen$ino2024'; // Substitua pela senha do seu banco de dados
+$dbData->options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$dbData->connection = new PDO($dbData->driver . ':host=' . $dbData->host . ';port=' . $dbData->port . ';dbname=' . $dbData->dbname, $dbData->user, $dbData->password, $dbData->options);
 ?>
