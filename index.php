b4ef32130cf6061b6146f7c3996dd714213aedd5
Ester Castro <111320314+estersdecastro@users.noreply.github.com>
2024-04-19 16:01:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();  ?>
 
     <h1>Banco De Dados UFPA</h1>
     <br>
-    <p><a href="/home/site/wwwroot/signup.php">Cadastre-se</a></p>
+    <p><a href="signup.php">Cadastre-se</a></p>
     <p><a href="login.php">Faça Login</a></p>
 
 </body>
